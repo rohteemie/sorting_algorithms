b699abfd35bc58e191e441b3eee9c6bf5f132b70
Rotimi Adebowale Owolabi <iamrotimiowolabi@gmail.com>
2022-08-14 22:56:16 +0100

    codeFix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,7 +1,27 @@
 #include "sort.h"
 
+/**
+* bubble_sort - a function that sorts an array of integers in
+* ascending order using the Bubble sort algorithm
+* @array: array of integers
+* @size: size of array
+**/
+
 void bubble_sort(int *array, size_t size)
 {
-	/*Code Goes Here*/
-	
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size -1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+			}
+			print_array(array, size);
+		}
+		
+
+	}
 }
--- sort.h
+++ sort.h
@@ -1,8 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -22,6 +21,7 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void swap(int *a, int *b);
 
 
 #endif /**  SORT_H */
